15bc2a7269940fb7a2d3b8fe67eb601e7ec8a34a
kelvinrandu <randukelvin@gmail.com>
2019-05-30 17:40:56 +0300

    added the first cool script in python

---
+++ python/love.py
@@ -0,0 +1,2 @@
+# if you want to run this script navigate to this file and type python love.py to run this script
+print('\n'.join([''.join([('Love'[(x-y) % len('Love')] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0else' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
